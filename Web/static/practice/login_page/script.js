3cb24b2b04fe349c8339d19c43730312a9fa4a1e
gaogao-qwq <gaogaoqwq@gmail.com>
2023-01-12 22:44:17 +0800

    Add submit check

--- Web/static/practice/login_page/script.js
+++ Web/static/practice/login_page/script.js
@@ -1,29 +1,31 @@
+const container = document.getElementById('container')
 const registerSwitch = document.getElementById('register-switch')
 const loginSwitch = document.getElementById('login-switch')
-const container = document.getElementById('container')
+const loginButton = document.getElementById('login-button')
+const registerButton = document.getElementById('register-button')
 const registerForm = document.getElementById('register-form')
 const loginForm = document.getElementById('login-form')
 
-const switchTop = registerSwitch.getBoundingClientRect().top - loginForm.getBoundingClientRect().top + registerSwitch.getBoundingClientRect().height / 2
-const switchLeft = registerSwitch.getBoundingClientRect().left - loginForm.getBoundingClientRect().left + registerSwitch.getBoundingClientRect().width / 2
-
 registerSwitch.addEventListener('click', ()=>{
+	const registerSwitchTop = registerSwitch.getBoundingClientRect().top - loginForm.getBoundingClientRect().top + registerSwitch.getBoundingClientRect().height / 2
+	const reigsterSwitchLeft = registerSwitch.getBoundingClientRect().left - loginForm.getBoundingClientRect().left + registerSwitch.getBoundingClientRect().width / 2
 	let fillSpan = document.createElement('span')
 	let fadeSpan = document.createElement('span')
-	
-	fillSpan.style.top = switchTop + 'px'
-	fillSpan.style.left = switchLeft + 'px'
-	fadeSpan.style.top = switchTop + 'px'
-	fadeSpan.style.left = switchLeft + 'px'
 
 	loginForm.appendChild(fillSpan)
 	registerForm.appendChild(fadeSpan)
-	fillSpan.style.animation = 'circle-fill .5s ease-out forwards'
+	fillSpan.style.top = registerSwitchTop + 'px'
+	fillSpan.style.left = reigsterSwitchLeft + 'px'
+	fillSpan.style.animation = 'circle-fill .25s ease-out forwards'
 
 	fillSpan.addEventListener('animationend', ()=>{
 		registerForm.style.display = 'flex'
 		loginForm.style.display = 'none'
-		fadeSpan.style.animation = 'circle-fade .5s ease-out forwards'
+		const loginSwitchTop = loginSwitch.getBoundingClientRect().top - registerForm.getBoundingClientRect().top + loginSwitch.getBoundingClientRect().height / 2
+		const loginSwitchLeft = loginSwitch.getBoundingClientRect().left - registerForm.getBoundingClientRect().left + loginSwitch.getBoundingClientRect().width / 2
+		fadeSpan.style.top = loginSwitchTop + 'px'
+		fadeSpan.style.left = loginSwitchLeft + 'px'
+		fadeSpan.style.animation = 'circle-fade .25s ease-out forwards'
 		fillSpan.remove()
 	})
 	fadeSpan.addEventListener('animationend', ()=>{
@@ -32,22 +34,25 @@ registerSwitch.addEventListener('click', ()=>{
 })
 
 loginSwitch.addEventListener('click', ()=>{
+	const loginSwitchTop = loginSwitch.getBoundingClientRect().top - registerForm.getBoundingClientRect().top + loginSwitch.getBoundingClientRect().height / 2
+	const loginSwitchLeft = loginSwitch.getBoundingClientRect().left - registerForm.getBoundingClientRect().left + loginSwitch.getBoundingClientRect().width / 2
 	let fillSpan = document.createElement('span')
 	let fadeSpan = document.createElement('span')
-
-	fillSpan.style.top = switchTop + 'px'
-	fillSpan.style.left = switchLeft + 'px'
-	fadeSpan.style.top = switchTop + 'px'
-	fadeSpan.style.left = switchLeft + 'px'
-
+	
 	loginForm.appendChild(fadeSpan)
 	registerForm.appendChild(fillSpan)
-	fillSpan.style.animation = 'circle-fill .5s ease-out forwards'
+	fillSpan.style.top = loginSwitchTop + 'px'
+	fillSpan.style.left = loginSwitchLeft + 'px'
+	fillSpan.style.animation = 'circle-fill .25s ease-out forwards'
 
 	fillSpan.addEventListener('animationend', ()=>{
 		registerForm.style.display = 'none'
 		loginForm.style.display = 'flex'
-		fadeSpan.style.animation = 'circle-fade .5s ease-out forwards'
+		const registerSwitchTop = registerSwitch.getBoundingClientRect().top - loginForm.getBoundingClientRect().top + registerSwitch.getBoundingClientRect().height / 2
+		const reigsterSwitchLeft = registerSwitch.getBoundingClientRect().left - loginForm.getBoundingClientRect().left + registerSwitch.getBoundingClientRect().width / 2
+		fadeSpan.style.top = registerSwitchTop + 'px'
+		fadeSpan.style.left = reigsterSwitchLeft + 'px'
+		fadeSpan.style.animation = 'circle-fade .25s ease-out forwards'
 		fillSpan.remove()
 	})
 	fadeSpan.addEventListener('animationend', ()=>{
