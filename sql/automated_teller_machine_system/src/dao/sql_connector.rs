accd8b8dc4da579896ec2d47e70d17a6488c97c9
gaogao-qwq <gaogaoqwq@gmail.com>
2023-12-24 20:38:45 +0800

    Chore: Add a function to pre run 'USE' statement befor return the PooledConn
    Signed-off-by: gaogao-qwq <gaogaoqwq@gmail.com>

--- sql/automated_teller_machine_system/src/dao/sql_connector.rs
+++ sql/automated_teller_machine_system/src/dao/sql_connector.rs
@@ -1,4 +1,4 @@
-use mysql::{Pool, PooledConn};
+use mysql::{Pool, PooledConn, prelude::Queryable};
 use once_cell::sync::OnceCell;
 use url::Url;
 
@@ -13,6 +13,13 @@ impl SqlConnector {
     pub fn get_conn() -> PooledConn {
         return CONNECTOR.get().expect("SqlConnector not initialized yet")
             .pool.get_conn().expect("Failed to get connection from pool");
+    }
+
+    pub fn get_bank_system_conn() -> PooledConn {
+        let mut conn =  CONNECTOR.get().expect("SqlConnector not initilized yet")
+            .pool.get_conn().expect("Failed to get connection from pool");
+        conn.query_drop("USE bank_system").expect("Failed to get bank_system database");
+        return conn;
     }
 
     pub fn init_conn() -> std::result::Result<(), Box<dyn std::error::Error>> {
